52f74547fab301aa1b197e5717c5ce6088ee7c1f
DevExpressExampleBot <fakeEmail>
2023-09-06 11:03:51 +0400

    Source auto update [skip ci]

--- VB/XtraTabControl_CustomButtons/Form1.Designer.vb
+++ VB/XtraTabControl_CustomButtons/Form1.Designer.vb
@@ -1,65 +1,62 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace XtraTabControl_CustomButtons
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.xtraTabControl1 = New DevExpress.XtraTab.XtraTabControl()
-			CType(Me.xtraTabControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' xtraTabControl1
-			' 
-			Me.xtraTabControl1.ClosePageButtonShowMode = DevExpress.XtraTab.ClosePageButtonShowMode.InActiveTabPageHeaderAndOnMouseHover
-			Me.xtraTabControl1.CustomHeaderButtons.AddRange(New DevExpress.XtraTab.Buttons.CustomHeaderButton() { New DevExpress.XtraTab.Buttons.CustomHeaderButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Plus), New DevExpress.XtraTab.Buttons.CustomHeaderButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Minus)})
-			Me.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.xtraTabControl1.Location = New System.Drawing.Point(0, 0)
-			Me.xtraTabControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-			Me.xtraTabControl1.Name = "xtraTabControl1"
-			Me.xtraTabControl1.Size = New System.Drawing.Size(558, 333)
-			Me.xtraTabControl1.TabIndex = 0
-'			Me.xtraTabControl1.SelectedPageChanged += New DevExpress.XtraTab.TabPageChangedEventHandler(Me.xtraTabControl1_SelectedPageChanged);
-'			Me.xtraTabControl1.CloseButtonClick += New System.EventHandler(Me.xtraTabControl1_CloseButtonClick);
-'			Me.xtraTabControl1.CustomHeaderButtonClick += New DevExpress.XtraTab.ViewInfo.CustomHeaderButtonEventHandler(Me.xtraTabControl1_CustomHeaderButtonClick);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(558, 333)
-			Me.Controls.Add(Me.xtraTabControl1)
-			Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-			Me.Name = "Form1"
-			Me.Text = "XtraTabControl Custom Buttons"
-			CType(Me.xtraTabControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.xtraTabControl1 = New DevExpress.XtraTab.XtraTabControl()
+            CType((Me.xtraTabControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' xtraTabControl1
+            ' 
+            Me.xtraTabControl1.ClosePageButtonShowMode = DevExpress.XtraTab.ClosePageButtonShowMode.InActiveTabPageHeaderAndOnMouseHover
+            Me.xtraTabControl1.CustomHeaderButtons.AddRange(New DevExpress.XtraTab.Buttons.CustomHeaderButton() {New DevExpress.XtraTab.Buttons.CustomHeaderButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Plus), New DevExpress.XtraTab.Buttons.CustomHeaderButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Minus)})
+            Me.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.xtraTabControl1.Location = New System.Drawing.Point(0, 0)
+            Me.xtraTabControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+            Me.xtraTabControl1.Name = "xtraTabControl1"
+            Me.xtraTabControl1.Size = New System.Drawing.Size(558, 333)
+            Me.xtraTabControl1.TabIndex = 0
+            AddHandler Me.xtraTabControl1.SelectedPageChanged, New DevExpress.XtraTab.TabPageChangedEventHandler(AddressOf Me.xtraTabControl1_SelectedPageChanged)
+            AddHandler Me.xtraTabControl1.CloseButtonClick, New System.EventHandler(AddressOf Me.xtraTabControl1_CloseButtonClick)
+            AddHandler Me.xtraTabControl1.CustomHeaderButtonClick, New DevExpress.XtraTab.ViewInfo.CustomHeaderButtonEventHandler(AddressOf Me.xtraTabControl1_CustomHeaderButtonClick)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(558, 333)
+            Me.Controls.Add(Me.xtraTabControl1)
+            Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+            Me.Name = "Form1"
+            Me.Text = "XtraTabControl Custom Buttons"
+            CType((Me.xtraTabControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents xtraTabControl1 As DevExpress.XtraTab.XtraTabControl
-	End Class
+#End Region
+        Private xtraTabControl1 As DevExpress.XtraTab.XtraTabControl
+    End Class
 End Namespace
-
--- VB/XtraTabControl_CustomButtons/Form1.vb
+++ VB/XtraTabControl_CustomButtons/Form1.vb
@@ -1,4 +1,3 @@
-Imports Microsoft.VisualBasic
 Imports System.Drawing
 Imports System.IO
 Imports System.Windows.Forms
@@ -10,66 +9,71 @@ Imports DevExpress.XtraTab.ViewInfo
 Imports System
 
 Namespace XtraTabControl_CustomButtons
-	Partial Public Class Form1
-		Inherits XtraForm
-		Public Sub New()
-			InitializeComponent()
-			xtraTabControl1.CustomHeaderButtons(0).ToolTip = "Add Image..."
-			xtraTabControl1.CustomHeaderButtons(1).ToolTip = "Remove Image"
-			AddImage("Start Page", ResourceImageHelper.CreateImageFromResources("XtraTabControl_CustomButtons.08.jpg", GetType(Form1).Assembly))
-			xtraTabControl1.TabPages(0).ShowCloseButton = DefaultBoolean.False
-		End Sub
-		Private Sub xtraTabControl1_CustomHeaderButtonClick(ByVal sender As Object, ByVal e As CustomHeaderButtonEventArgs) Handles xtraTabControl1.CustomHeaderButtonClick
-			If e.Button.Kind = ButtonPredefines.Plus Then
-				OnAddImageBtnClick()
-			End If
-			If e.Button.Kind = ButtonPredefines.Minus Then
-				OnRemoveImageClick()
-			End If
-		End Sub
-		Private Sub OnAddImageBtnClick()
-			Using ofd As New OpenFileDialog()
-				ofd.Filter = "All Picture Files |*.bmp;*.gif;*.jpg;*.jpeg;*.ico;*.png|Text Files |*.txt;*.rtf"
-				ofd.Title = "Open"
-				ofd.Filter = ""
-				If ofd.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
-					LoadImage(ofd.FileName)
-				End If
-			End Using
-		End Sub
-		Private Sub OnRemoveImageClick()
-			xtraTabControl1.SelectedTabPage.Dispose()
-			UpdateButtons()
-		End Sub
-		Private Sub UpdateButtons()
-			'xtraTabControl1.CustomHeaderButtons[1].Enabled = xtraTabControl1.TabPages.Count > 1;
-		End Sub
-		Private Sub LoadImage(ByVal path As String)
-			If File.Exists(path) Then
-				AddImage(path, Image.FromFile(path))
-			End If
-		End Sub
-		Private Sub AddImage(ByVal caption As String, ByVal img As Image)
-			Dim page As New XtraTabPage()
-			page.Text = caption
-			Dim picEdit As New PictureEdit()
-			picEdit.Image = img
-			picEdit.Dock = DockStyle.Fill
-			picEdit.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
-			picEdit.Properties.PictureAlignment = ContentAlignment.MiddleCenter
-			picEdit.Properties.SizeMode = PictureSizeMode.Squeeze
-			picEdit.Parent = page
-			xtraTabControl1.TabPages.Add(page)
-			xtraTabControl1.SelectedTabPage = page
-			UpdateButtons()
-		End Sub
-		Private Sub xtraTabControl1_CloseButtonClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles xtraTabControl1.CloseButtonClick
-			Dim ea As ClosePageButtonEventArgs = TryCast(e, ClosePageButtonEventArgs)
-			CType(ea.Page, IDisposable).Dispose()
-		End Sub
 
-		Private Sub xtraTabControl1_SelectedPageChanged(ByVal sender As Object, ByVal e As TabPageChangedEventArgs) Handles xtraTabControl1.SelectedPageChanged
-			'xtraTabControl1.CustomHeaderButtons[1].Enabled = (e.Page.Text != "Start Page");
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            xtraTabControl1.CustomHeaderButtons(0).ToolTip = "Add Image..."
+            xtraTabControl1.CustomHeaderButtons(1).ToolTip = "Remove Image"
+            AddImage("Start Page", ResourceImageHelper.CreateImageFromResources("XtraTabControl_CustomButtons.08.jpg", GetType(Form1).Assembly))
+            xtraTabControl1.TabPages(0).ShowCloseButton = DefaultBoolean.False
+        End Sub
+
+        Private Sub xtraTabControl1_CustomHeaderButtonClick(ByVal sender As Object, ByVal e As CustomHeaderButtonEventArgs)
+            If e.Button.Kind = ButtonPredefines.Plus Then OnAddImageBtnClick()
+            If e.Button.Kind = ButtonPredefines.Minus Then OnRemoveImageClick()
+        End Sub
+
+        Private Sub OnAddImageBtnClick()
+            Using ofd As OpenFileDialog = New OpenFileDialog()
+                ofd.Filter = "All Picture Files |*.bmp;*.gif;*.jpg;*.jpeg;*.ico;*.png|Text Files |*.txt;*.rtf"
+                ofd.Title = "Open"
+                ofd.Filter = ""
+                If ofd.ShowDialog() = DialogResult.OK Then
+                    LoadImage(ofd.FileName)
+                End If
+            End Using
+        End Sub
+
+        Private Sub OnRemoveImageClick()
+            xtraTabControl1.SelectedTabPage.Dispose()
+            UpdateButtons()
+        End Sub
+
+        Private Sub UpdateButtons()
+        'xtraTabControl1.CustomHeaderButtons[1].Enabled = xtraTabControl1.TabPages.Count > 1;
+        End Sub
+
+        Private Sub LoadImage(ByVal path As String)
+            If File.Exists(path) Then
+                AddImage(path, Image.FromFile(path))
+            End If
+        End Sub
+
+        Private Sub AddImage(ByVal caption As String, ByVal img As Image)
+            Dim page As XtraTabPage = New XtraTabPage()
+            page.Text = caption
+            Dim picEdit As PictureEdit = New PictureEdit()
+            picEdit.Image = img
+            picEdit.Dock = DockStyle.Fill
+            picEdit.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
+            picEdit.Properties.PictureAlignment = ContentAlignment.MiddleCenter
+            picEdit.Properties.SizeMode = PictureSizeMode.Squeeze
+            picEdit.Parent = page
+            xtraTabControl1.TabPages.Add(page)
+            xtraTabControl1.SelectedTabPage = page
+            UpdateButtons()
+        End Sub
+
+        Private Sub xtraTabControl1_CloseButtonClick(ByVal sender As Object, ByVal e As EventArgs)
+            Dim ea As ClosePageButtonEventArgs = TryCast(e, ClosePageButtonEventArgs)
+            CType(ea.Page, IDisposable).Dispose()
+        End Sub
+
+        Private Sub xtraTabControl1_SelectedPageChanged(ByVal sender As Object, ByVal e As TabPageChangedEventArgs)
+        'xtraTabControl1.CustomHeaderButtons[1].Enabled = (e.Page.Text != "Start Page");
+        End Sub
+    End Class
 End Namespace
